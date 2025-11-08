b71e9df65afcc09d641965c5e08c7e223b917f7e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-08 21:15:47 +0800

    Backend connection

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'dtree' => [
+        'api_url' => env('DTREE_API', 'https://backendmarc-12854796833.europe-west1.run.app'),
+    ],
+
 ];
